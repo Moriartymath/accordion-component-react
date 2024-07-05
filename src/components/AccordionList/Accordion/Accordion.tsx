d091f487f93efb2d3387a184466e90ec03fe0f66
Moriarty <116695498+Moriartymath@users.noreply.github.com>
2024-07-05 22:54:11 +0300

    Add functionality of openening QA - Add styling

--- src/components/AccordionList/Accordion/Accordion.tsx
+++ src/components/AccordionList/Accordion/Accordion.tsx
@@ -8,15 +8,28 @@ type AccordionProps = {
 
 function Accordion({ qaItem, qaNumber }: AccordionProps) {
   const [isOpen, setIsOpen] = useState(false);
+  const textColor = isOpen ? "#00b700" : "";
 
   return (
-    <div className={styles.container}>
-      <h3 className={styles.qaNumber}>0{qaNumber}</h3>
+    <div
+      className={`${styles.container} ${isOpen ? styles.opened : ""}`}
+      onClick={() => setIsOpen((currIsOpen) => !currIsOpen)}
+    >
+      <h3
+        className={styles.qaNumber}
+        style={{ color: isOpen ? textColor : "#80808096" }}
+      >
+        0{qaNumber}
+      </h3>
       <main className={styles.mainContainer}>
-        <h3 className={styles.title}>{qaItem.title}</h3>
-        <p className={styles.text}>{qaItem.text}</p>
+        <h3 className={styles.title} style={{ color: textColor }}>
+          {qaItem.title}
+        </h3>
+        {isOpen ? <p className={styles.text}>{qaItem.text}</p> : null}
       </main>
-      <span className={styles.sign}>{!isOpen ? "+" : "-"}</span>
+      <span className={styles.sign}>
+        {!isOpen ? <>&#x2b;</> : <>&minus;</>}
+      </span>
     </div>
   );
 }
